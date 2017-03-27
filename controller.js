ae1da3075693fed5079f106d019e1b2964e0b6c3
Jonathan Darling <jmdarling@me.com>
2017-03-27 11:38:23 -0500

    Style fixes to meet standard specifications.

--- controller.js
+++ controller.js
@@ -1,19 +1,19 @@
 module.exports = (app, playerService) => {
-    app.get('/', (request, response) => {
-        response.send('Sick bruh')
-    })
+  app.get('/', (request, response) => {
+    response.send('Sick bruh')
+  })
 
-    app.get('/players', (request, response) => {
+  app.get('/players', (request, response) => {
     const players = request.query.players.split(',')
 
     console.log(`\n\n-----Recieved request for players: ${players.join(', ')}-----`)
 
     Promise.all(players.map(playerService.getUserInfo))
-        .then(playerStats => {
-            response.send(playerStats)
-        })
-        .catch(error => {
-            response.status(500).send(error)
-        })
-    })
+      .then(playerStats => {
+        response.send(playerStats)
+      })
+      .catch(error => {
+        response.status(500).send(error)
+      })
+  })
 }
